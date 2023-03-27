a23ff10407f22ccbaa418312fd85199a697e60d5
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-03-27 17:01:45 -0300

    Views, Text, styles

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello, world!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Lorem ipsum dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+        <Text>Lorem ipsum dolor sit amet</Text>
+      </View>
     </View>
   );
 }
@@ -16,5 +22,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
   },
 });
